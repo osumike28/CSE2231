60e0b59d6d2e57f6f5bd845059772afec475510f
osumike28 <harper.kicker8@gmail.com>
2023-09-21 06:44:17 -0400

    Test with one new method and Comments

--- MapWithHashing/test/MapTest.java
+++ MapWithHashing/test/MapTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 
 
 
-
 /**
  * JUnit test fixture for {@code Map<String, String>}'s constructor and kernel
  * methods.
@@ -84,6 +83,9 @@ public abstract class MapTest {
         return map;
     }
 
+    /**
+     * Test No Argument Constructor
+     */
     @Test 
     public final void testNoArgumentConstructor() {
         // creates test maps
@@ -93,54 +95,83 @@ public abstract class MapTest {
         // test
         assertEquals(map, mapExpected);
     }
-   
+    
+   /**
+    *  Test adding a single pair
+    */
     @Test 
     public final void testAddSingle() {
         // creates test maps
         Map<String, String> map = this.constructorTest();
         Map<String, String> mapExpected = this.createFromArgsRef("O,H");
         
+        // adds pair to map
         map.add("O", "H");
         
         // test
         assertEquals(map, mapExpected);
         
     }
+    
+    /**
+     * Test adding multiple pairs
+     */
     @Test 
     public final void testAddMultiple() {
         // creates test maps
         Map<String, String> map = this.constructorTest();
         Map<String, String> mapExpected = this.createFromArgsRef("O,H","1","O");
         
+        //adds pairs to map
         map.add("O", "H");
         map.add("1", "O");
         
-        // test
+        // tests
         assertEquals(map, mapExpected);     
     }
     
-    
+    /**
+     * Test removing single pair
+     */
     @Test 
     public final void testRemoveSingle() {
         // creates test maps 
         Map<String, String> map = this.createFromArgsRef("O,H");
         Map<String, String> mapExpected = this.constructorRef();
         
+        // removes pair from map
         map.remove("O");
 
         // test
-        assertEquals(map, mapExpected);
-        
-        
-    }   
+        assertEquals(map, mapExpected);  
+    }  
     
+    /**
+     * Test removing any value pair
+     */
+    @Test
+    public final void testRemoveAny() {
+        // creates test maps 
+        Map<String, String> map = this.createFromArgsTest("O", "H", "1", "O");
+        int initialSize = map.size();
+        
+        // removes pair from map
+        map.removeAny();
+        
+        //tests
+        assertEquals(initialSize - 1, map.size());
+    }
     
+    /**
+     * Test removing multiple value pair
+     */
     @Test 
     public final void testRemoveMultiple() {
         // creates test maps
         Map<String, String> map = this.createFromArgsTest("O", "H", "1", "O");
         Map<String, String> mapExpected = this.constructorRef();
         
+        // removes multiple pairs
         map.remove("O");
         map.remove("1");
         
@@ -148,13 +179,15 @@ public abstract class MapTest {
         assertEquals(map, mapExpected);     
     }
     
+    /**
+     * Test retrieving value for key
+     */
     @Test
     public final void testValueSingle() {
-     // creates test maps
+        // creates test maps
         Map<String, String> m = this.createFromArgsTest("O", "H");
-        
-        
-         
+    
+        // Expected and retrieved value
         String valueExpected = "H";
         String  value = m.value("O");
         
@@ -162,20 +195,34 @@ public abstract class MapTest {
         assertEquals(value, valueExpected);
     }
     
+    /**
+     * Test if there is a key
+     */
     @Test
     public final void testHasKeyTrue() {
+        // creates test maps 
         Map<String, String> m = this.createFromArgsTest("O", "H");
         
+        //test
         assertTrue(m.hasKey("O"));
     }
     
+    /**
+     * Tests the size of map
+     */
     @Test
     public final void testSizeMultiple() {
+        // creates test maps 
         Map<String, String> m = this.createFromArgsTest("O", "H", "I", "O");
+        
+        // Expected and retrieved value
         int sizeExpected = 2;
         int size= m.size();
+        
+        // test
         assertEquals(sizeExpected, size);
     }
     
 }
+
 
